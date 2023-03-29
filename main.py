52340acfc9702b043896c8437281d65364b35f3e
Danylo Anatoliev <dannilwastaken@knu.ua>
2023-03-29 10:11:00 +0300

    Number of sentences

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-f = open("text.txt", "r")
-sentence = ''
+import re
 
-for x in f:
-  sentence += x
+# Writing text file into a string
+filename = "text.txt"
+with open(filename, 'r') as file:
+    sentence = file.read()
 
-f.close()
-
+# String output
 print(sentence)
+
+# Getting number of sentences
+def numberOfSentences(sentence):
+    sentences = re.findall(r'[\w\s]*[\.\?!]+|[\w\s]*\.{3}', sentence)
+    num_sentences = len(sentences)
+    return num_sentences
+
+print(f'Number of sentences is: {numberOfSentences(sentence)}')
